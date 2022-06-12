a44b55213ff44428e06ac643dbbd46c5a6228026
Michael Meoak <mmeoak240@gmail.com>
2022-06-12 11:23:54 -0700

    commit tree 49127b0245616f5783827bfcdaa28766f93fd684

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,29 @@
-# Write your code here.
+
+def badge_maker name
+"Hello, my name is #{name}."
+end
+
+def batch_badge_creator names
+names.map do |name|
+  "Hello, my name is #{name}."
+  end
+
+end
+
+def assign_rooms names
+x = 0
+names.map do |name|
+  x += 1
+  "Hello, #{name}! You'll be assigned to room #{x}!"
+end
+end
+
+def printer(attendees)
+  batch_badge_creator(attendees).each do |badge|
+    puts badge
+  end
+
+  assign_rooms(attendees).each do |assignment|
+    puts assignment
+  end
+end
